e8dd91fbfdd98a71704cb501f985ab35dfffcde2
Chmouss <cyprien.lek@hotmail.com>
2025-12-20 23:14:24 +0100

    init day 6

--- src/Day5part2.java
+++ src/Day5part2.java
@@ -14,7 +14,7 @@ public class Day5part2 {
             Scanner fileReader = new Scanner(file);
 
             //map => Key = start of the range, Value = end of the range
-            HashMap<Long, Long> mapRangeStartToEnd = new HashMap<>();
+            HashMap<Long, Long> mapStartRangeToEndRange = new HashMap<>();
 
             long totalOfFreshIngredients = 0;
 
@@ -27,8 +27,17 @@ public class Day5part2 {
 
                 //split the line between 2 long (in the txt file they are separated by a - )
                 String[] ranges = currentLine.split("-");
-                long rangeStart = Long.parseLong(ranges[0]);
-                long rangeEnd = Long.parseLong(ranges[1]);
+                long newNumberRangeStart = Long.parseLong(ranges[0]);
+                long newNumberRangeEnd = Long.parseLong(ranges[1]);
+
+                boolean isContainedWithinOrEquals = false;
+                boolean isNewEntryOutstandingTheMapEntry = false;
+                boolean isOverlappingAtTheEnd = false;
+                boolean isOverlappingAtTheStart = false;
+
+                long mapEntryToDelete = 0;
+                long mapEntryToAddIfOverlap = 0;
+
 
 
                 //note to work on this later =>
@@ -44,7 +53,54 @@ public class Day5part2 {
 
                 //note for the equals in the overlap ->
                 // equals or lesser/greater for the number that is BETWEEN both, other one is stricly greater or lesser
+
+
+
+                //check existing entries in the map
+                for (Map.Entry<Long, Long> entry: mapStartRangeToEndRange.entrySet()){
+                    long mapEntryStart = entry.getKey();
+                    long mapEntryEnd = entry.getValue();
+
+                    if(newNumberRangeStart >= mapEntryStart && newNumberRangeEnd <= mapEntryEnd){
+                        isContainedWithinOrEquals = true;
+                        break;
+                    }
+                    if(newNumberRangeStart < mapEntryStart && newNumberRangeEnd > mapEntryEnd){
+                        //replace that entry with the new one
+                        mapEntryToDelete = mapEntryStart;
+                        isNewEntryOutstandingTheMapEntry = true;
+                        break;
+                    } else if (newNumberRangeStart < mapEntryStart && (newNumberRangeEnd >= mapEntryStart && newNumberRangeEnd <= mapEntryEnd)) {
+                        isOverlappingAtTheEnd = true;
+                        mapEntryToAddIfOverlap = mapEntryStart -1;
+                        break;
+                    } else if (newNumberRangeEnd > mapEntryEnd && (newNumberRangeStart >= mapEntryStart && newNumberRangeStart <= mapEntryEnd)){
+                        isOverlappingAtTheStart = true;
+                        mapEntryToAddIfOverlap = mapEntryEnd +1;
+                        break;
+                    }
+                }
+
+                //if both values are outstanding the map entry, we replace that one
+                if(isNewEntryOutstandingTheMapEntry){
+                    mapStartRangeToEndRange.remove(mapEntryToDelete);
+                    mapStartRangeToEndRange.put(newNumberRangeStart, newNumberRangeEnd);
+                } else if(isOverlappingAtTheEnd){
+                    mapStartRangeToEndRange.put(newNumberRangeStart, mapEntryToAddIfOverlap);
+                } else if(isOverlappingAtTheStart){
+                    mapStartRangeToEndRange.put(mapEntryToAddIfOverlap, newNumberRangeEnd);
+                } else if (!isContainedWithinOrEquals){ //if it is all good, we simply add it to the map
+                    mapStartRangeToEndRange.put(newNumberRangeStart, newNumberRangeEnd);
+                }
+
             }
+
+            for (Map.Entry<Long, Long> map: mapStartRangeToEndRange.entrySet()){
+                long mapKey = map.getKey();
+                long mapValue = map.getValue();
+                totalOfFreshIngredients += (mapValue - mapKey) +1; //ex: 3-5 => 3, 4 and 5 are valid IDs
+            }
+            System.out.println(totalOfFreshIngredients);
 
         } catch (FileNotFoundException e) {
             System.out.println("error while reading the file");
